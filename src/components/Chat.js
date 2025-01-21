24210c9f01dd4ea1ddfeca2867a9c554d0396134
Eugene Toder <eltoder@gmail.com>
2025-01-21 00:33:29 -0500

    Revert "Replace js code with css scroll-behavior: smooth"

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -49,7 +49,6 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
     overflowWrap: "anywhere",
     padding: "0 4px 4px 4px",
-    scrollBehavior: "smooth",
   },
   vertIcon: {
     marginLeft: "auto",
--- src/utils/autoscroll.js
+++ src/utils/autoscroll.js
@@ -2,31 +2,56 @@
 // https://medium.com/@heatherbooker/how-to-auto-scroll-to-the-bottom-of-a-div-415e967e7a24
 
 export default function autoscroll(element) {
-  let timer = null,
-    atBottom = true;
+  function animateScroll(duration) {
+    var start = element.scrollTop;
+    var end = element.scrollHeight;
+    var change = end - start;
+    var increment = 20;
+
+    function easeInOut(currentTime, start, change, duration) {
+      // by Robert Penner
+      currentTime /= duration / 2;
+      if (currentTime < 1) {
+        return (change / 2) * currentTime * currentTime + start;
+      }
+      currentTime -= 1;
+      return (-change / 2) * (currentTime * (currentTime - 2) - 1) + start;
+    }
+
+    function animate(elapsedTime) {
+      elapsedTime += increment;
+      var position = easeInOut(elapsedTime, start, change, duration);
+      element.scrollTop = position;
+      if (elapsedTime < duration) {
+        setTimeout(function () {
+          animate(elapsedTime);
+        }, increment);
+      }
+    }
+
+    animate(0);
+  }
+
+  var fullyScrolled = true;
 
   function onScroll() {
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-      // https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollHeight#determine_if_an_element_has_been_totally_scrolled
-      atBottom =
-        Math.abs(this.scrollHeight - this.clientHeight - this.scrollTop) <= 1;
-    }, 100);
+    // https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollHeight#determine_if_an_element_has_been_totally_scrolled
+    fullyScrolled =
+      Math.abs(this.scrollHeight - this.clientHeight - this.scrollTop) <= 1;
   }
 
   function scrollToBottom() {
-    if (atBottom) {
-      element.scrollTop = element.scrollHeight;
-    }
+    var duration = 300;
+    if (fullyScrolled) animateScroll(duration);
   }
 
-  const observer = new MutationObserver(scrollToBottom);
-  observer.observe(element, { childList: true });
+  var observer = new MutationObserver(scrollToBottom);
+  var config = { childList: true };
+  observer.observe(element, config);
   element.scrollTop = element.scrollHeight;
   element.addEventListener("scroll", onScroll);
   return () => {
     observer.disconnect();
     element.removeEventListener("scroll", onScroll);
-    clearTimeout(timer);
   };
 }
