8d21b75fa3e0d99dacf4984a11a35d7042342954
Eugene Toder <eltoder@gmail.com>
2025-01-23 15:47:28 -0500

    Replace js code with css scroll-behavior: smooth
    Also, debounce onScroll event handler.

--- src/utils/autoscroll.js
+++ src/utils/autoscroll.js
@@ -2,56 +2,41 @@
 // https://medium.com/@heatherbooker/how-to-auto-scroll-to-the-bottom-of-a-div-415e967e7a24
 
 export default function autoscroll(element) {
-  function animateScroll(duration) {
-    var start = element.scrollTop;
-    var end = element.scrollHeight;
-    var change = end - start;
-    var increment = 20;
-
-    function easeInOut(currentTime, start, change, duration) {
-      // by Robert Penner
-      currentTime /= duration / 2;
-      if (currentTime < 1) {
-        return (change / 2) * currentTime * currentTime + start;
-      }
-      currentTime -= 1;
-      return (-change / 2) * (currentTime * (currentTime - 2) - 1) + start;
-    }
-
-    function animate(elapsedTime) {
-      elapsedTime += increment;
-      var position = easeInOut(elapsedTime, start, change, duration);
-      element.scrollTop = position;
-      if (elapsedTime < duration) {
-        setTimeout(function () {
-          animate(elapsedTime);
-        }, increment);
-      }
-    }
-
-    animate(0);
-  }
-
-  var fullyScrolled = true;
+  let atBottom = true,
+    lastAutoScroll = 0,
+    timer = null;
 
   function onScroll() {
-    // https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollHeight#determine_if_an_element_has_been_totally_scrolled
-    fullyScrolled =
-      Math.abs(this.scrollHeight - this.clientHeight - this.scrollTop) <= 1;
+    clearTimeout(timer);
+    const now = Date.now();
+    if (now - lastAutoScroll < 100) {
+      lastAutoScroll = now; // still auto-scrolling
+    } else {
+      timer = setTimeout(() => {
+        // https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollHeight#determine_if_an_element_has_been_totally_scrolled
+        atBottom =
+          Math.abs(this.scrollHeight - this.clientHeight - this.scrollTop) <= 1;
+      }, 100);
+    }
   }
 
-  function scrollToBottom() {
-    var duration = 300;
-    if (fullyScrolled) animateScroll(duration);
-  }
+  element.scrollToBottom = () => {
+    atBottom = true;
+    lastAutoScroll = Date.now();
+    element.scrollTop = element.scrollHeight;
+  };
 
-  var observer = new MutationObserver(scrollToBottom);
-  var config = { childList: true };
-  observer.observe(element, config);
-  element.scrollTop = element.scrollHeight;
+  const observer = new MutationObserver(() => {
+    if (atBottom) {
+      element.scrollToBottom();
+    }
+  });
+  observer.observe(element, { childList: true });
   element.addEventListener("scroll", onScroll);
+  element.scrollToBottom();
   return () => {
     observer.disconnect();
     element.removeEventListener("scroll", onScroll);
+    clearTimeout(timer);
   };
 }
