29deedea58169da0601311561b94bf63fce64970
Eugene Toder <eltoder@gmail.com>
2025-06-01 07:56:16 -0400

    Remove dragon boat logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -106,7 +106,7 @@ function Navbar() {
       <Toolbar variant="dense">
         <Typography variant="h6" style={{ flexGrow: 1, whiteSpace: "nowrap" }}>
           <InternalLink underline="none" color="inherit" to="/">
-            跟朋友一起玩SET 🐉🚣
+            Set with Friends
           </InternalLink>
         </Typography>
         <Typography
