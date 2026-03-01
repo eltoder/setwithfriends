a571823296665ef0aeddc101edd098663fd1f00e
Eugene Toder <eltoder@gmail.com>
2026-03-01 17:57:06 -0500

    Reapply "Replace moment with date-fns (#196)"
    This reverts commit 7228b0e92ad9766e27eb539c18a33ee7f14143b9.

--- src/components/ElapsedTime.js
+++ src/components/ElapsedTime.js
@@ -1,9 +1,12 @@
+import formatDistanceStrict from "date-fns/formatDistanceStrict";
+
 import useMoment from "../hooks/useMoment";
 
 // Wrapper around useMoment, since state hooks cause rerender of component
 function ElapsedTime({ value }) {
-  const time = useMoment();
-  return <>{time.to(value)}</>;
+  const time = useMoment(5000);
+  const opts = { addSuffix: true };
+  return <>{formatDistanceStrict(value, time, opts)}</>;
 }
 
 export default ElapsedTime;
